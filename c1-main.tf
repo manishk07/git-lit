9d849b17e427737e7a0231d10e158c4188a89667
Manish Singh <manishk07@live.in>
2024-07-23 17:58:36 +0530

    Test dependency lock, random provider and ignored tfstates

--- c1-main.tf
+++ c1-main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_version = ">1.0.0"
   #required_version = "~> 0.14.3" ~ means it allows only the rightmost version component to increment
-  
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -10,20 +10,14 @@ terraform {
       #version = ">=2.0.0, <=2.64.0"
       #version = "~2.64" #For Production Grade
     }
-  }
-}
 
-#Provider-1 for Central India (default Provider
-provider "azurerm" {
-  features {}
-}
-
-#Provider-2 for Australia East
-provider "azurerm" {
-  features {
-    virtual_machine {
-      delete_os_disk_on_deletion = false
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.0.0"
     }
   }
-  alias = "provider2-australiaeast"
+}
+
+provider "azurerm" {
+  features {}
 }
--- c2-rg.tf
+++ c2-rg.tf
@@ -1,12 +1,25 @@
-#Uses default provider
-resource "azurerm_resource_group" "rg-centralindia" {
-  name     = "rg-centralindia"
+#Create Resource Group
+resource "azurerm_resource_group" "myrg1" {
+  name     = "myrg-1"
   location = "Central India"
 }
 
-#Demonstrating multiple providers in TF
-resource "azurerm_resource_group" "rg-australiaeast" {
-  name     = "rg-australiaeast"
-  location = "Australia East"
-  provider = azurerm.provider2-australiaeast
+#Create Random String
+resource "random_string" "myrandom" {
+  length  = 6
+  upper   = false
+  special = false
+}
+
+#Create a Storage Account
+resource "azurerm_storage_account" "mystore" {
+  name                     = "mystorage${random_string.myrandom.id}"
+  location                 = azurerm_resource_group.myrg1.location
+  resource_group_name      = azurerm_resource_group.myrg1.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Dept = "IT"
+  }
 }
